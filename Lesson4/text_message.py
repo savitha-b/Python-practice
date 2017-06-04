1f59bfe91aeefdcde94cdf7c116363586443a517
Savitha Bhallamudi <savitha.bhallamudi@gmail.com>
2017-06-04 18:39:54 -0400

    Update text_message.py

--- Lesson4/text_message.py
+++ Lesson4/text_message.py
@@ -2,18 +2,21 @@
 #example: validatio texts from gmail/other websites, texting without having phone number etc.
 #created a twilio account
 
-from twilio.rest import Client
+#from twilio.rest import Client - the original code calls the class Client fro the rest folder of __init__.py
+#we replace this with the following line
+from twilio import rest #inside code twilio, ther's another folder called rest, we call that forlder.
 
 # Your Account SID from twilio.com/console
-account_sid = "AC71bc20dc5ba851ca493905396dada85d"
+account_sid = "ACxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 # Your Auth Token from twilio.com/console
-auth_token  = "902ebe20e3121d85e6c03c7a129a2adc"
+auth_token  = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
-client = Client(account_sid, auth_token)
+#client = Client(account_sid, auth_token)  - access the class acc to old code
+client = rest.Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+14808233655",  #replace with your phone number
-    from_="+12408835505", #replace with your twilio number
+    to="+1480xxxxxxx",  #replace with your phone number
+    from_="+1240xxxxxxx", #replace with your twilio number
     body="Hello from Python!")
 
 print(message.sid)
