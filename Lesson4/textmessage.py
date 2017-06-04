61855288970f6e50c9d68dac5a1380b851438c90
Savitha Bhallamudi <savitha.bhallamudi@gmail.com>
2017-06-04 13:21:19 -0400

    Delete textmessage.py

--- Lesson4/textmessage.py
+++
@@ -1,2 +0,0 @@
-#python package to send text messages: twilio, not available in python standard library. Has to be downloaded externally.
-#example: validatio texts from gmail/other websites, texting without having phone number etc.
