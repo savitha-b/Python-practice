0b486fbd9ece65f5bff889728985a9f61ef4892f
Savitha Bhallamudi <savitha.bhallamudi@gmail.com>
2017-06-05 02:51:18 -0400

    class definition for parent

---
+++ Lesson6/inheritance/inheritance.py
@@ -0,0 +1,6 @@
+#class definition
+class Parent():
+    def __init__(self, last_name, eye_color):
+        print('Parent Constructor called')
+        self.last_name = last_name
+        self.eye_color = eye_color
