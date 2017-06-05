a11158e3997aec99425a3ed39d4b21441eb6355f
Savitha Bhallamudi <savitha.bhallamudi@gmail.com>
2017-06-05 03:20:03 -0400

    add function show_info to parent class

--- Lesson6/inheritance/inheritance.py
+++ Lesson6/inheritance/inheritance.py
@@ -4,7 +4,11 @@ class Parent():
         print('Parent Constructor called')
         self.last_name = last_name
         self.eye_color = eye_color
-        
+
+    def show_info(self):
+        print("Last Name -"+self.last_name)
+        print("Eye Color -"+self.eye_color)
+
 #define class child
 class Child(Parent): #syntax to show child can reuse anything publicly available in class parent
     def __init__(self, last_name, eye_color, number_of_toys):
@@ -13,10 +17,11 @@ class Child(Parent): #syntax to show child can reuse anything publicly available
         self.number_of_toys = number_of_toys
         
 #test by creating an instance
-#billy_cyrus = Parent("Cyrus", "blue")
+billy_cyrus = Parent("Cyrus", "blue")
 #print(billy_cyrus.last_name)
+billy_cyrus.show_info()
 
 #instance for class child
-miley_cyrus = Child("Cyrus", "blue",5)
-print(miley_cyrus.last_name)
-print(miley_cyrus.number_of_toys)
+#miley_cyrus = Child("Cyrus", "blue",5)
+#print(miley_cyrus.last_name)
+#print(miley_cyrus.number_of_toys)
